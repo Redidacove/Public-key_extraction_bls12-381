4c3b3efe8e35ca054889686cd4fe51d399cad1a4
Md Amaan <Redidacove>
2024-03-15 17:08:35 +0530

    Code push

---
+++ src/bin/main.rs
@@ -0,0 +1,20 @@
+use lambdaworks_math::{
+    cyclic_group::IsGroup,
+};
+use lambdaworks_math::elliptic_curve::{
+    short_weierstrass::curves::bls12_381::curve::BLS12381Curve,
+    traits::IsEllipticCurve,
+};
+// use lambdaworks_math::elliptic_curve::traits::IsEllipticCurve;
+
+fn main(){
+    let g = BLS12381Curve::generator();
+    let sk:u64=0x6C616D6264617370;
+    g.operate_with_self(sk);
+    let g2=g.to_affine();
+    let x=g2.x();
+    let y=g2.y();
+    
+    println!("{}",x);
+    println!("{}",y);
+}
